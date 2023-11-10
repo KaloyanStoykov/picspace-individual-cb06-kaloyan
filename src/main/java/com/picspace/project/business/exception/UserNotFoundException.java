a12cd5714c870d7625ad7b9c202fcf9a5673688f
Kaloyan Stoykov <k.stoykov@student.fontys.nl>
2023-11-10 11:10:18 +0100

    change yml

--- src/main/java/com/picspace/project/business/exception/UserNotFoundException.java
+++ src/main/java/com/picspace/project/business/exception/UserNotFoundException.java
@@ -3,8 +3,9 @@ package com.picspace.project.business.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class UnderageUserException extends RuntimeException {
-    public UnderageUserException(){
-        super("User is underage (under 18 years old)");
+public class UserNotFoundException extends ResponseStatusException {
+
+    public UserNotFoundException(){
+        super(HttpStatus.NOT_FOUND, "User not found!");
     }
 }
