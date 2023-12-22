e9f15ba99d87d8c846c340073c74a75920e3abc0
Kaloyan Stoykov <k.stoykov@student.fontys.nl>
2023-12-22 23:34:56 +0100

    re-run unit-tests for sonar

--- src/main/java/com/picspace/project/business/dbConverter/EntryConverter.java
+++ src/main/java/com/picspace/project/business/dbConverter/EntryConverter.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EntryConverter implements EntityConverter<EntryEntity, Entry> {
-
-
     private EntityConverter<UserEntity, User> userConverter;
 
 
